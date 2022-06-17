12594f6039b84d64fd34b8dbae36905700828528
PanchiRivas <105081657+PanchiRivas@users.noreply.github.com>
2022-06-17 17:21:48 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <NavBar></NavBar>
+</template>
+
+<script>
+
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+
+#app{
+  width: 100%;
+  
+}
+
+
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-navbar id="nav">
+      <h1 id="titlenav" >ToDoer</h1>
+      <img src="@/assets/seleccione.png" class="d-inline-block align-top" id="logo" alt="logo">
+  </b-navbar>
+
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+
+#nav{
+  width: 100%;
+  background-color:#facbfb;
+  box-shadow: 1px 0px 9px 6px rgba(71, 9, 85, 0.68);
+}
+#logo{
+  width: 50px;
+  margin-right: 5%;
+}
+#titlenav{
+  text-align: left;
+  width: 100%;
+  font-size: 40px;
+  margin-left: 5%;
+}
+
+
+</style>
+
