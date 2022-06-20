e9ecaad08d572f804002e9582a960d9f8b1313d1
PanchiRivas <105081657+PanchiRivas@users.noreply.github.com>
2022-06-20 04:04:26 -0400

    Update CurrentTask.vue

--- src/components/CurrentTask.vue
+++ src/components/CurrentTask.vue
@@ -6,12 +6,20 @@
             </b-col>
             <b-col cols="8" lg="8" md="8">
                 <div class="mt-3" v-for="task of tasks" v-bind:key="task">
-                    <p>{{task.text}}</p>
-                    <div class="buttons">
-                        <b-button variant="success">O</b-button>
-                        <b-button variant="danger">X</b-button>
-                    </div>
-                    
+
+                    <b-row>
+                        <b-col cols="7" sm="8" lg="8" md="8" xl="9"><p>{{task.text}}</p></b-col>
+                        <b-col cols="1" sm="1" lg="1" md="1" xl="1">
+                            
+                        </b-col>
+                        <b-col cols="4" sm="3" lg="3" md="3" xl="2">
+                            <div class="right">
+                            <b-button variant="success">O</b-button>
+                            <b-button variant="danger">X</b-button>
+                            </div>
+                        </b-col>
+                    </b-row>
+
                 </div>
             </b-col>
             <b-col cols="2" lg="2" md="2">
@@ -48,17 +56,5 @@ export default {
     border-radius: 2px;
     box-shadow: 0px 0px 3px rgba(66,66,66,.76);
 }
-.buttons{
-    display: inline-flex;
-}
-.btn{
-    outline:none !important;
-    border: none;
-    background: none;
-}
-p{
-    width: 92%;
-    margin: 0;
-    display: inline-flex;
-}
+
 </style>
